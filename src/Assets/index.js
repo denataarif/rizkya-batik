68ce1bb8aab6e50db133113829e8679af1bcf490
Denata Arif Nur Muhamad <denataarif@gmail.com>
2024-09-18 00:07:32 +0700

    dropdown :)

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -2,12 +2,15 @@ import Logo from './Img/Logo Rizkya Baatik.svg'
 import Search from './Icons/Search.svg'
 import Cart from './Icons/Cart (2).svg'
 import Profile from './Icons/Profile (2).svg'
-
+import Dropdown from './Icons/Dropdown.svg'
+import activeDropdown from './Icons/Dropdown-active.svg'
 const assets = {
     Logo,
     Search,
     Cart,
-    Profile
+    Profile,
+    Dropdown,
+    activeDropdown
 }
 
 export default assets
--- src/Component/molecules/Navbar/Navbar.jsx
+++ src/Component/molecules/Navbar/Navbar.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import assets from '../../../Assets'
 import Typhograph from '../../atoms/Typhograph/Typhograph'
+import Select from '../../atoms/Select/Select'
 const Navbar = () => {
+  const [isHover, Setishover] = useState(false)
+  const Catagories = ["NEW IN", "WOMEN", "MEN", "REGULAR COLLECTION", "DRESS", "OUTWEAR"]
   return (
     <>
         <div className='w-full bg-black py-5 flex justify-around items-center'>
             <div className="Logo rounded-[97px]"><img src={assets.Logo} alt='logo'className='w-[150px] h-[72px]'/></div>
-            <div className="Catagories flex justify-around items-center ">
+            <div className="Catagories flex justify-around items-center gap-x-4">
                 <Typhograph children="NEW DROP" className="text-primary hover:text-isActive hover:cursor-pointer"/>
-                <Typhograph children="SHOP ALL" className="text-primary mx-4 hover:text-isActive hover:cursor-pointer"/>
-                <Typhograph children="HIGHLIGHT" className="text-primary mr-4 hover:text-isActive hover:cursor-pointer"/>
-                <Typhograph children="CATAGORIES" className="text-primary hover:text-isActive hover:cursor-pointer"/>
+                <Typhograph children="SHOP ALL" className="text-primary hover:text-isActive hover:cursor-pointer"/>
+                <Typhograph children="HIGHLIGHT" className="text-primary hover:text-isActive hover:cursor-pointer"/>
+                <div className='flex gap-x-2 items-center hover:cursor-pointer text-primary hover:text-isActive focus:text-isActive'>
+                  <select name="" id="" className='bg-transparent border-none hover:border-none focus:outline-none active:outline-none font-normal' >
+                    <option selected hidden >CATAGOREIS</option>
+                    {
+                      Catagories.map((items, index) => {
+                        return(
+                          <option key={index} className='bg-black'>
+                            {items}
+                          </option>
+                        )
+                      })
+                    }
+                  </select>
+                </div>
+                <Select/>
             </div>
-            <div className="Icons flex justify-between items-center ">
+            <div className="Icons flex justify-between items-center gap-x-6">
                 <img src={assets.Search} alt="Icon-Search" className='h-6 w-6'/>
-                <img src={assets.Cart} alt="Icon-Cart" className='h-6 w-6 mx-8'/>
+                <img src={assets.Cart} alt="Icon-Cart" className='h-6 w-6'/>
                 <img src={assets.Profile} alt="Icon-Profile" className='h-6 w-6'/>
             </div>
         </div>
